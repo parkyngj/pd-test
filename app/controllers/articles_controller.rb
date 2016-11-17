29909b58da7c3b8d64c73c8feabd9d2848c30cbc
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-17 11:30:53 -0800

    Add dependency, destroy button for article works, saving form params work in progress

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to root
+    redirect_to '/'
   end
 
   private
