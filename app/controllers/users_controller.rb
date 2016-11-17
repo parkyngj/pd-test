58c6d56afc0633011cfb6d74316c7e5474302576
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-17 13:11:04 -0800

    Header for logging in and registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
-class UrlController < ApplicationController
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = current_user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
