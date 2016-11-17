ea87b521c2b029c7a8f77a68e3ca51f01b6f0a9f
Derrick Carr <derrick.the.future@gmail.com>
2016-11-17 10:16:24 -0800

    Update user model and articles migration to use the friendlier 'references' syntax.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, foreign_key: :author_id
   has_many :revisions, foreign_key: :editor_id
   has_many :edited_articles, through: :revisions, source: :article
   # Include default devise modules. Others available are:
