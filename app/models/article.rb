8fc92bc4f002becc86a57dce598a448dc0c073a8
Derrick Carr <derrick.the.future@gmail.com>
2016-11-16 21:22:20 -0800

    Fix a :bug: in migration and models for foreign keys

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,10 @@
 class Article < ApplicationRecord
-  belongs_to :author, class_name: :user
+  belongs_to :author, class_name: :User
   has_many :footnotes
   has_many :photos
   has_many :bibliographies
   has_many :article_categories
   has_many :revisions
   has_many :categories, through: :article_categories
-  has_many :editors, through: :revisions, source: :user
+  has_many :editors, through: :revisions
 end
--- db/migrate/20161117004319_create_revisions.rb
+++ db/migrate/20161117004319_create_revisions.rb
@@ -1,7 +1,7 @@
 class CreateRevisions < ActiveRecord::Migration[5.0]
   def change
     create_table :revisions do |t|
-      t.references :user, foreign_key: true
+      t.integer :editor_id
       t.references :article, foreign_key: true
     end
   end
