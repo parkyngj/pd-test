14a1983f5af842394177e0fd373124302f620076
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-16 20:30:37 -0800

    Add footnote routes

--- app/controllers/footnotes_controller.rb
+++ app/controllers/footnotes_controller.rb
@@ -1,2 +1,46 @@
 class FootnotesController < ActionController::Base
+
+  def new
+    @article = Article.find(params[:article_id])
+    @footnote = @article.footnotes.new
+  end
+
+  def create
+    @article = Article.find(params[:id])
+    @footnote = @article.footnotes.new(footnote_params)
+
+    if @footnote.save
+      redirect_to article_path(@article, @footnote)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:article_id])
+    @footnote = @article.footnotes.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @footnote = @article.footnotes.find(params[:id])
+
+    if @footnote.update(footnote_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @footnote = @article.footnotes.find(params[:id])
+    @footnote.destroy
+    redirect_to @article
+  end
+
+  private
+    def footnote_params
+      params.require(:footnote).permit(:text, :url)
+    end
 end
