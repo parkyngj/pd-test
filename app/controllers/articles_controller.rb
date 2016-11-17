128ef14b27d297c64343362a6e6647930d30909f
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-16 20:15:23 -0800

    Add routes for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,50 @@
 class ArticlesController < ActionController::Base
 
+  def index
+    @articles = Article.all
+  end
 
-end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+end 
