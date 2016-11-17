a4bd7f31230bb5b93fe7c558800e60724f8b0d14
Derrick Carr <derrick.the.future@gmail.com>
2016-11-16 21:25:27 -0800

    Revert to friendlier syntax for migrations.

--- db/migrate/20161117004319_create_revisions.rb
+++ db/migrate/20161117004319_create_revisions.rb
@@ -1,7 +1,7 @@
 class CreateRevisions < ActiveRecord::Migration[5.0]
   def change
     create_table :revisions do |t|
-      t.integer :editor_id
+      t.references :user, foreign_key: true
       t.references :article, foreign_key: true
     end
   end
