265d8ef413600da68e0a79953b116e81f9c02db3
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-17 18:36:26 -0800

    Add routes for revisions

--- app/controllers/concerns/revisions_controller.rb
+++ app/controllers/concerns/revisions_controller.rb
@@ -1,2 +1,12 @@
-index
-show
+class ArticlesController < ApplicationController
+
+  def index
+    @article = Article.find(params[:article_id])
+    @revision = @article.revisions
+  end
+
+  def show
+    @revisions = Revision.find(params[:id])
+  end
+
+end
